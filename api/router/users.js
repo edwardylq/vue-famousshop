feed571af34eee613ecf09a147577e9e7be98ca6
edwardylq <570886364@qq.com>
2018-03-14 12:00:45 +0800

    搜索

--- api/router/users.js
+++ api/router/users.js
@@ -19,7 +19,7 @@ module.exports = {
                 res.send(apiResult(result && result.length > 0,result));
             })
         })
-    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         app.post('/backlogin',(req,res)=>{
             let name=req.body.username;
             let pwd =String(req.body.password);
@@ -34,5 +34,37 @@ module.exports = {
                 res.send(apiResult(result && result.length > 0, token));
             })
         })
+
+                //模糊搜索
+                
+        app.get('/product',(req,res)=>{
+            var _type = req.query.type
+            var _style = req.query.style
+            console.log(req.query)
+            // db.mongodb.select('product',{_type,_style}).then((result)=>{
+            // db.getCollection('product').find({style:_style}||{ type:_type}).then((res)=>{
+            //     if(res){
+            //         res.send((apiResult(true,res,'success',null)));
+            //     }else{
+            //         res.send(apiResult(false));
+            //     }
+                 
+            // })
+            for(var attr in req.query){
+                req.query[attr] =  {$regex:req.query[attr]}
+
+            }
+
+            console.log(req.query)
+            db.mongodb.select('product',req.query).then((result) => {
+                
+                res.send(apiResult(result && result.length > 0,result));
+            })
+
+        })
+
+
+
     }
+
 }
