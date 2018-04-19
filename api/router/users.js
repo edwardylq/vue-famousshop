9723af9b3173223cfa474018c29327a73086ca51
edwardylq <570886364@qq.com>
2018-04-19 18:57:02 +0800

    done

--- api/router/users.js
+++ api/router/users.js
@@ -7,9 +7,9 @@ module.exports = {
     register(app){
         app.post('/login', (req, res) => {
             let username = req.body.username;
-            let password = req.body.password;
-            console.log(username)
-            db.mongodb.select('product').then((result) => {
+            let pwd = req.body.password;
+            // console.log(username,pwd);
+            db.mongodb.select('regs',{username,pwd}).then((result) => {
                 let token = '';
                 var user = {username};
                 if(result.length > 0){
@@ -17,7 +17,7 @@ module.exports = {
                         'expiresIn': 1440 // 设置过期时间
                     })
                 }
-                res.send(apiResult(result && result.length > 0,result));
+                res.send(apiResult(result && result.length > 0,token));
             })
         })
         // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -88,6 +88,46 @@ module.exports = {
                 res.send(result);
             })
         })
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        app.post('/getusers',(req,res)=>{
+             let params=JSON.parse(req.body.params);
+            let pageno=params.pageno;
+            let qty=params.qty;
+            db.mongodb.select('regs').then(result=>{
+                var _result=result.slice((pageno-1)*qty,qty*pageno);
+                let total=Math.ceil(result.length/qty);
+                res.send(apiResult(true,_result,total));
+            })
+        })
+
+         app.post('/editusers',(req,res)=>{
+            let params=JSON.parse(req.body.params);
+            let id=params.a0;
+            let oid=db.mongodb.objectid(id);
+            let username=params.a1;
+            let pwd=params.a2;
+            // console.log(id,username,pwd);
+            db.mongodb.update('regs',{"_id":oid},{username,pwd}).then(result=>{
+                res.send(result);
+            })
+        })
+
+         app.post('/delusers',(req,res)=>{
+            let id=req.body.id;
+            let oid=db.mongodb.objectid(id);
+            db.mongodb.delete('regs',{"_id":oid}).then((result)=>{
+                res.send({status:true,data:result});
+            }) 
+        })
+
+         app.post('/addusers',(req,res)=>{
+            let params=JSON.parse(req.body.params);
+            // console.log(params);
+            db.mongodb.insert('regs',params).then(result=>{
+                res.send(result);
+            })
+        })
+
 
     // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -102,7 +142,7 @@ module.exports = {
 
             }
 
-            console.log(req.query)
+            // console.log(req.query)
             db.mongodb.select('product',req.query).then((result) => {
                 
                 res.send(apiResult(result && result.length > 0,result));
@@ -120,7 +160,7 @@ module.exports = {
 
             }
 
-            console.log(req.query)
+            // console.log(req.query)
             db.mongodb.sortup('product',req.query).then((result) => {
                 
                 res.send(apiResult(result && result.length > 0,result));
@@ -132,13 +172,13 @@ module.exports = {
          app.get('/qty',(req,res)=>{
             var _type = req.query.type
             var _style = req.query.style
-            console.log(req.query)
+            // console.log(req.query)
             for(var attr in req.query){
                 req.query[attr] =  {$regex:req.query[attr]}
 
             }
 
-            console.log(req.query)
+            // console.log(req.query)
             db.mongodb.qtyup('product',req.query).then((result) => {
                 
                 res.send(apiResult(result && result.length > 0,result));
@@ -146,7 +186,151 @@ module.exports = {
 
         })
 
+          app.post('/register', (req, res) => {
+            let username = req.body.username;
+            let pwd = req.body.password;
+            // console.log(username,pwd);
+            db.mongodb.select('regs',{username}).then((result) => {
+                if(result&&result.length>0){
+                    res.send(apiResult(false));
+                }else{
+                    db.mongodb.insert('regs',{username,pwd}).then((result)=>{
+                        res.send(result);
+                    })
+                }
+            })
+        })
 
+        
+
+        app.post('/getallproducts',(req,res)=>{
+            db.mongodb.select('product').then(result=>{
+                res.send(result)
+            })
+        })
+
+        
+        app.post('/shoppingcar', (req, res) => {
+            let url = req.body.url;
+            let qty = req.body.qty*1;
+            console.log(qty);
+            let goodname = req.body.goodname;
+            let username = req.body.username;
+            let price = req.body.price;
+            // res.send({url,goodname})
+            db.mongodb.select('shoppingcart',{url,username,goodname}).then((result) => { 
+                console.log(result)
+                // res.send(result);
+                if(result&&result.length>0){console.log(qty);
+                    var num=result[0].qty*1+qty;
+                    db.mongodb.update('shoppingcart',{url},{qty:num}).then((res1)=>{console.log(666)
+                        res.send(res1);
+                    })
+                }else{
+                    db.mongodb.insert('shoppingcart',{url,qty,goodname,price,username}).then((res2)=>{
+                         res.send(res2);
+                    })
+                }
+               
+            })
+        })
+
+
+         app.post('/sercar',(req,res)=>{
+            let username=req.body.username
+            console.log(username);
+            db.mongodb.select('shoppingcart',{username}).then((res1)=>{
+                res.send(res1);
+            })
+        })
+        // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        app.post('/operation',(req,res)=>{
+            let equal=req.body.equal;
+            let username=req.body.username;
+            let url =req.body.url;
+            console.log(username,url,equal);
+            db.mongodb.select('shoppingcart',{username,url}).then(result=>{
+                console.log(result);
+                let qty=null;
+                if(equal=='a'){
+                    qty=(result[0].qty)*1+1;
+                }else{
+                    qty=(result[0].qty)*1-1;
+                }
+                console.log(result[0].qty,qty);
+                db.mongodb.update('shoppingcart',{username,url},{qty:qty}).then(result=>{
+                    // console.log(result);
+                     res.send(result);
+                });
+            });
+        })
+
+        app.post('/enter',(req,res)=>{
+            let username=req.body.username;
+            let url =req.body.url;
+            let qty=req.body.qty;
+             db.mongodb.select('shoppingcart',{username,url}).then(result=>{
+                // console.log(result);
+                db.mongodb.update('shoppingcart',{username,url},{qty:qty}).then(result=>{
+                    res.send(result);
+                });
+             });
+        })
+
+        app.post('/delcarproducts',(req,res)=>{
+            // let username=String(req.body.username);
+            // let url=String(req.body.url);
+            // console.log(username,url);
+            // let _id = res.body.aa;
+            let id=req.body.id;
+            let _id=db.mongodb.objectid(id);
+            db.mongodb.delete('shoppingcart',{_id}).then(result=>{
+                res.send(result);
+            })
+        })
+
+
+        app.post('/payment',(req,res)=>{
+            let username=req.body.username;
+            db.mongodb.select('shoppingcart',{username}).then(result=>{
+                console.log(result);
+                let total=0;
+                result.map(function(item){
+                    total+=item.price*1;
+                    return true;
+                });
+                let order={
+                    orderno:parseInt(Math.random()*10000000),
+                    total,
+                    status:0,
+                    products:result
+                }
+                // console.log(total,result);
+                db.mongodb.insert('order',order).then((result)=>{
+                    if(result.result.ok==1){
+                        res.send({status:true,result});
+                    }else{
+                        res.send({status:false,error:'加入数据库失败'});
+                    }
+                });
+            });
+        })
+
+        app.post('/getcarorder',(req,res)=>{
+            let orderno=Number(req.body.orderno);
+            console.log(orderno);
+            db.mongodb.select('order',{orderno}).then(result=>{
+                res.send(result);
+            })
+        })
+
+        app.post('/delallcarproducts',(req,res)=>{
+            let username=req.body.username;
+            console.log(username);
+            db.mongodb.delete('shoppingcart',{username}).then(result=>{
+                res.send(result);
+            })
+        })
     }
 
 }
